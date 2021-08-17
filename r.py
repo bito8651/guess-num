b3ea9ef079389350a0146de4bfee8dfbc5d6b737
SI-YU <bito8651@gmail.com>
2021-08-17 15:53:23 +0800

    final version

--- r.py
+++ r.py
@@ -1,10 +1,14 @@
 import random
+start = input('請決定隨機數字範圍開始值： ')
+end = input('請決定隨機數字範圍結束值： ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100) # 產生範圍1~100的隨機整數
+r = random.randint(start, end) 
 count = 0
 while True:
     count += 1 # count = count + 1 的快寫法
-    num = input('請猜1~100的數字： ')
+    num = input('請猜數字： ')
     num = int(num)
     if num == r:
         print('你猜對了！')
